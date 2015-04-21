1bc6ecf64119b5450846f6486602ee81350856c7
Alejandro Echeverria <alejandro@echev.com>
2015-04-20 22:45:08 -0300

    Added formatting to binary

--- GBSharp.ViewModel/MemoryWordViewModel.cs
+++ GBSharp.ViewModel/MemoryWordViewModel.cs
@@ -1,51 +1,55 @@
-﻿namespace GBSharp.ViewModel
+﻿using System;
+
+namespace GBSharp.ViewModel
 {
   public class MemoryWordViewModel : ViewModelBase
   {
     private readonly int _address;
     private readonly int _value;
 
-    private string _addressFormatString = string.Empty;
-    private string _valueFormatString = string.Empty;
+    private string _addressFormatString;
+    private string _valueFormatString;
 
     public string Address
     {
-      get { return _address.ToString(_addressFormatString); }
+      get { return _addressFormatString; }
     }
 
     public string Value
     {
-      get { return _value.ToString(_valueFormatString); }
+      get { return _valueFormatString; }
     }
 
     public MemoryWordViewModel(int address, int value)
     {
       _address = address;
+      _addressFormatString = address.ToString();
       _value = value;
+      _valueFormatString = value.ToString();
     }
 
     public void UpdateAddressFormat(MemoryWordFormat addressFormat)
     {
-      _addressFormatString = GetFormat(addressFormat);
+      _addressFormatString = GetFormat(addressFormat, _address);
       OnPropertyChanged(() => Address);
     }
 
     public void UpdateValueFormat(MemoryWordFormat valueFormat)
     {
-      _valueFormatString = GetFormat(valueFormat);
+      _valueFormatString = GetFormat(valueFormat, _value);
       OnPropertyChanged(() => Value);
     }
 
-    private string GetFormat(MemoryWordFormat format)
+    private string GetFormat(MemoryWordFormat format, int value)
     {
       switch (format)
       {
         case MemoryWordFormat.Decimal:
-          return "";
+          return "" + value;
         case MemoryWordFormat.Hexa:
-          return "x2";
+          return "0x" + value.ToString("x2");
         case MemoryWordFormat.Binary:
-          return "";
+          return Convert.ToString(value, 2);
 
       }
       return "";
