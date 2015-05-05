91114bb3bc5754c839f803f61e836d5c5a505297
cristiandonosoc <cristiandonosoc@gmail.com>
2015-05-04 23:52:44 -0400

    Added (initial) version of MemoryHandlerFactory

---
+++ GBSharp/Memory/MemoryHandlers/MemoryHandlerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GBSharp.Catridge;
+
+namespace GBSharp.Memory.MemoryHandlers
+{
+    class MemoryHandlerFactory
+    {
+        /// <summary>
+        /// Creates a MemoryHandler the matches the CartridgeType.
+        /// </summary>
+        /// <param name="cartridge">The cartridge to be loaded into the GB</param>
+        /// <returns>A MemoryHandler instance</returns>
+        public static MemoryHandler CreateMemoryHandler(Cartridge cartridge)
+        {
+            // TODO(Cristián): Implement logic for different CartridgeType
+            return new RomOnlyMemoryHandler(cartridge);
+        }
+    }
+}
