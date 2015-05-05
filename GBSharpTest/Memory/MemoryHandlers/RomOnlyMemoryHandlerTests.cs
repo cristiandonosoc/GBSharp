3c45352f924251008bf1f3809f16cc0364a2feed
cristiandonosoc <cristiandonosoc@gmail.com>
2015-05-04 23:53:31 -0400

    Added RomOnlyMemoryHandler Test

---
+++ GBSharpTest/Memory/MemoryHandlers/RomOnlyMemoryHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GBSharp.Catridge;
+using GBSharp.Memory.MemoryHandlers;
+
+namespace GBSharpTest.Memory.MemoryHandlers
+{
+  [TestClass]
+  public class RomOnlyMemoryHandlerTests
+  {
+    [TestMethod]
+    public void RomOnlyMemoryHandlerWriteWorks()
+    {
+      // Arrange
+      Cartridge cartridge = new Cartridge();
+      cartridge.Load(new byte[65536]);
+      RomOnlyMemoryHandler handler = new RomOnlyMemoryHandler(cartridge);
+      handler.LoadInternalMemory(new byte[65536]);
+
+      // Act
+      handler.Write(0x1001, 0x22);
+
+      // Assert
+      Assert.AreEqual<byte>(0x22, handler.Read(0x1001));
+    }
+  }
+}
