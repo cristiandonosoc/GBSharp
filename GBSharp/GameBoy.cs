07e14f833cabcb22397e97f54832cc1ec88100c3
Mauricio Castro <mauricio@wooo.cl>
2015-05-04 23:19:21 -0300

    Adds run, pause and stop interface to gameboy

--- GBSharp/GameBoy.cs
+++ GBSharp/GameBoy.cs
@@ -1,6 +1,8 @@
-﻿using GBSharp.Catridge;
+﻿using System.Threading;
+using GBSharp.Catridge;
 using GBSharp.CPU;
 using GBSharp.Memory;
+using System;
 
 namespace GBSharp
 {
@@ -9,6 +11,9 @@ namespace GBSharp
     private CPU.CPU cpu;
     private Memory.Memory memory;
     private Cartridge cartridge;
+    private bool run;
+    private Thread clockThread;
+    private ManualResetEventSlim manualResetEvent;
 
     /// <summary>
     /// Class constructor.
@@ -16,9 +21,12 @@ namespace GBSharp
     /// </summary>
     public GameBoy()
     {
+      this.run = false;
       this.memory = new Memory.Memory();
       this.cpu = new CPU.CPU(this.memory);
       this.cartridge = new Cartridge();
+      this.manualResetEvent = new ManualResetEventSlim(false);
+      this.clockThread = new Thread(new ThreadStart(this.Step));
     }
 
     public ICPU CPU
@@ -36,9 +44,67 @@ namespace GBSharp
       get { return cartridge; }
     }
 
+    /// <summary>
+    /// Loads 
+    /// </summary>
+    /// <param name="cartridgeData"></param>
     public void LoadCartridge(byte[] cartridgeData)
     {
       this.cartridge.Load(cartridgeData);
+    }
+
+    /// <summary>
+    /// Runs the simulation for the smallest amount of time possible.
+    /// This should be 1 whole instruction, arbitrary machine and clock cycles.
+    /// </summary>
+    public void Step()
+    {
+      // this.cpu.Step();
+    }
+
+    /// <summary>
+    /// Attempts to run until the heat death of the universe.
+    /// </summary>
+    public void Run()
+    {
+      this.run = true;
+      this.manualResetEvent.Set();
+      this.clockThread.Start();
+    }
+    
+    /// <summary>
+    /// Pauses the simulation until Run is called again.
+    /// </summary>
+    public void Pause()
+    {
+      // Do not change this.run to false or simulation will be stopped!
+      this.manualResetEvent.Reset();
+    }
+
+    /// <summary>
+    /// Cancels the current simulation. The internal state of the cpu and memory is discarded.
+    /// </summary>
+    public void Stop()
+    {
+      this.run = false; // Allow the thread to exit.
+      this.manualResetEvent.Set(); // Unlock the thread to make that happen.
+      
+      // Dispose CPU and memory, create a new one and load rom again?
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Method that is going to be running in a separate thread, calling Step() forever.
+    /// </summary>
+    private void ThreadedRun()
+    {
+      while (this.run)
+      {
+        this.manualResetEvent.Wait();
+        this.Step();
+
+        // TODO: Check here timing issues
+      }
     }
   }
 }
