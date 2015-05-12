8519990ba6b49b0c4fa3d9b9fed578a3b4165618
Alejandro Echeverria <alejandro@echev.com>
2015-05-11 22:54:03 -0300

    Das shift and die rotaten

--- GBSharp/Utils/Utils.cs
+++ GBSharp/Utils/Utils.cs
@@ -1,4 +1,7 @@
-﻿namespace GBSharp.Utils
+﻿using System;
+using Microsoft.Win32;
+
+namespace GBSharp.Utils
 {
   enum Flags
   {
@@ -20,6 +23,77 @@
     {
       byte mask = (byte)~(1 << bit);
       return (byte)(word & mask);
+    }
+
+    public static byte RotateLeft(byte value, int count = 1)
+    {
+      return (byte)((value << count) | (value >> (32 - count)));
+    }
+
+    public static byte RotateRight(byte value, int count = 1)
+    {
+      return (byte)((value >> count) | (value << (32 - count)));
+    }
+
+    public static Tuple<byte, byte> RotateLeftThroughCarry(byte value, int count = 1, int carry = 0)
+    {
+      var carryOut = (byte)((value >> 8) & 1);
+      var valueShifted = value << count;
+      var valueRotated = (byte)(valueShifted | carry);
+      return new Tuple<byte, byte>(valueRotated, carryOut);
+    }
+
+    public static Tuple<byte, byte> RotateLeftAndCarry(byte value, int count = 1)
+    {
+      var carryOut = (byte)((value >> 8) & 1);
+      var valueShifted = value << count;
+      var valueRotated = (byte)(valueShifted | carryOut);
+      return new Tuple<byte, byte>(valueRotated, carryOut);
+    }
+
+    public static Tuple<byte, byte> RotateRightThroughCarry(byte value, int count = 1, int carry = 0)
+    {
+      var carryOut = (byte)(value & 1);
+      var valueShifted = value >> count;
+      var valueRotated = (byte)(valueShifted | (carry << 8));
+      return new Tuple<byte, byte>(valueRotated, carryOut);
+    }
+
+    public static Tuple<byte, byte> RotateRightAndCarry(ushort value, int count = 1)
+    {
+      var carryOut = (byte)(value & 1);
+      var valueShifted = value >> count;
+      var valueRotated = (byte)(valueShifted | (carryOut << 8));
+      return new Tuple<byte, byte>(valueRotated, carryOut);
+    }
+
+    public static Tuple<byte, byte> ShiftLeft(byte value, int count = 1)
+    {
+      var carryOut = (byte)((value >> 8) & 1);
+      var valueShifted = (byte)(value << count);
+      return new Tuple<byte, byte>(valueShifted, carryOut);
+    }
+
+    public static Tuple<byte, byte> ShiftRightLogic(byte value, int count = 1)
+    {
+      var carryOut = (byte)(value & 1);
+      var valueShifted = value >> count;
+      return new Tuple<byte, byte>((byte)valueShifted, carryOut);
+    }
+
+    public static Tuple<byte, byte> ShiftRightArithmetic(ushort value, int count = 1)
+    {
+      var carryOut = (byte)(value & 1);
+      var msb = (byte)(value & 0x80);
+      var valueShifted = (byte) ((value >> count) | msb);
+      return new Tuple<byte, byte>(valueShifted, carryOut);
+    }
+
+    public static byte SwapNibbles(byte value)
+    {
+      var highNibble = (byte)(value >> 4);
+      var lowNibble = (byte)(value << 4);
+      return (byte)(highNibble | lowNibble);
     }
   }
 }
