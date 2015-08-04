b61891db2377c7f6ef13754088644a07283cc880
Cristián Donoso C <cristiandonosoc@gmail.com>
2015-08-03 23:20:20 -0300

    Added an iteration loop for display accessing over memory
    It correctly loops over the edges

--- GBSharp/VideoSpace/Display.cs
+++ GBSharp/VideoSpace/Display.cs
@@ -34,6 +34,83 @@ namespace GBSharp.VideoSpace
       //UpdateScreen();
     }
 
+    internal void UpdateScreen()
+    {
+      byte lcdRegister = this.memory.Read((ushort)MemoryMappedRegisters.LCDC);
+
+      // Tile memory address comes defined in LCD Control Register bit 3
+      // 0: 0x9800 - 0x9BFF
+      // 1: 0x9C00 - 0x9FFF
+      ushort displayBaseAddress = 0x9800;
+      if ((Utils.UtilFuncs.TestBit(lcdRegister, 3)) == 1)
+      {
+        displayBaseAddress = 0x9C00;
+      }
+
+      // Tile data base address is defined in LCD Control Register bit 4
+      // 0: 0x8800 - 0x97FF (signed access)
+      // 1: 0x8000 - 0x8FFF (unsigned access)
+      bool signedAccess = true;
+      ushort tileDataBaseAddress = 0x8800;
+      if ((Utils.UtilFuncs.TestBit(lcdRegister, 4)) == 1)
+      {
+        signedAccess = false;
+        tileDataBaseAddress = 0x8000;
+      }
+
+      // We interpret the pixels
+      // NOTE(Cristian): We use uint for the value, when byte would have
+      // sufficed (and even make the code a little slimmer). But this permits
+      // arbitrary sized screens which might prove useful while debugging.
+
+      // TODO(Cristian): initialX and initialY should be defined by the
+      // SCX and SCY registers respectively
+      uint initialX = 30;
+      uint initialY = 30;
+      uint screenWidth = 32;
+      uint screenHeight = 32;
+      uint displayWidth = 20;
+      uint displayHeight = 18;
+      uint currentX = initialX;   // Helper X variable to track overflow
+      uint currentY = initialY;   // Helper Y variable to track overflow
+
+      uint displayIndex = 0;      // The "pointer" to the actual pixel
+      uint firstOfRow = 0;        // To help the pointer go back to the start at overflow
+      for (int y = 0; y < displayHeight; y++)
+      {
+        // We move the index to the start of the display row
+        firstOfRow = currentY * screenHeight;
+        currentX = initialX;
+        displayIndex = (currentY * screenHeight) + currentX;
+
+        for (int x = 0; x < displayWidth; x++)
+        {
+          // We read the memory
+          // TODO(Cristian): Actually read the memory
+          Console.Out.WriteLine("{0}: Accessed: {1} (X: {2}, Y: {3})",
+            displayIndex, currentX, currentY);
+
+          displayIndex++;
+          currentX++;
+          if(currentX >= screenWidth)
+          {
+            currentX = 0;
+            displayIndex = firstOfRow;
+          }
+        }
+
+        Console.Out.WriteLine("ROW");
+
+        // We update the Y variable
+        currentY++;
+        if(currentY >= screenHeight)
+        {
+          currentY = 0;
+        }
+      }
+    }
+
+
     /// <summary>
     /// Simulates the update of the display for a period of time of a given number of ticks.
     /// </summary>
