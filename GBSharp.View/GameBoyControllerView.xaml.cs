4bd934734618877a37f93ae1e0b15ac47b0355b1
Alejandro Echeverria <alejandro@echev.com>
2015-08-17 22:32:33 -0300

    Modularizando los controles UI

---
+++ GBSharp.View/GameBoyControllerView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace GBSharp.View
+{
+  /// <summary>
+  /// Interaction logic for GameBoyControllerView.xaml
+  /// </summary>
+  public partial class GameBoyControllerView : UserControl
+  {
+    public GameBoyControllerView()
+    {
+      InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+      var openFileDialog = new OpenFileDialog();
+      openFileDialog.ShowDialog();
+
+      FileText.Text = openFileDialog.FileName;
+    }
+  }
+}
---
+++ GBSharp.ViewModel/DisplayViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace GBSharp.ViewModel
+{
+  public class DisplayViewModel : ViewModelBase
+  {
+    private readonly IDisplay _display;
+    
+    private BitmapImage _background;
+
+    public BitmapImage Background
+    {
+      get { return _background; }
+      set
+      {
+        _background = value;
+        OnPropertyChanged(() => Background);
+      }
+    }
+
+    public ICommand ReadCommand
+    {
+      get { return new DelegateCommand(CopyFromDomain); }
+    }
+
+    public ICommand WriteCommand
+    {
+      get { return new DelegateCommand(CopyToDomain); }
+    }
+
+    public DisplayViewModel(IDisplay display)
+    {
+      _display = display;
+    }
+
+    public void CopyFromDomain()
+    {
+      Background = Utils.BitmapToImageSource(_display.Background);
+    }
+
+    private void CopyToDomain()
+    {
+
+    }
+  }
+}
