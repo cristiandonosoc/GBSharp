8144d316105df6ac10452fc004fac3fabec4a0c7
Alejandro Echeverria <alejandro@echev.com>
2015-08-24 21:21:25 -0300

    Doing the memory

---
+++ GBSharp.ViewModel/MemorySectionViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace GBSharp.ViewModel
+{
+
+  public class MemorySectionViewModel : ViewModelBase
+  {
+    private readonly string _name;
+    private readonly int _initialAddress;
+    private readonly int _finalAddress;
+
+    public MemorySectionViewModel(string name, int initialAddress, int finalAddress)
+    {
+      _name = name;
+      _initialAddress = initialAddress;
+      _finalAddress = finalAddress;
+    }
+
+    public string Name
+    {
+      get { return _name; }
+    }
+
+    
+    public int InitialAddress
+    {
+      get { return _initialAddress; }
+    }
+
+    public int FinalAddress
+    {
+      get { return _finalAddress; }
+    }
+  }
+}
