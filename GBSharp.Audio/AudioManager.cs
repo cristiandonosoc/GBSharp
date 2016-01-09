4833df6664450e0c40b4acf392e564047223e1c0
Cristián Donoso C <cristiandonosoc@gmail.com>
2016-01-09 16:10:54 -0300

    Correctly handing sound stop/restart if gameboy stops
    In a very reset fashion, we delete the old state and recreate it
    lazily as needed.

--- GBSharp.Audio/AudioManager.cs
+++ GBSharp.Audio/AudioManager.cs
@@ -8,36 +8,30 @@ namespace GBSharp.Audio
 {
   public class AudioManager : IDisposable
   {
+    private IGameBoy _gameboy;
     private IAPU _apu;
-
     private WaveFormat _waveFormat;
-    //private CircularWriteableBufferingSource _source;
     private DirectStreamingSource _source;
-
     private ISoundOut _soundOut;
 
-
-    public AudioManager(IGameBoy gameBoy)
+    public AudioManager(IGameBoy gameboy)
     {
-      _apu = gameBoy.APU;
+      _gameboy = gameboy;
+      gameboy.FrameCompleted += gameBoy_FrameCompleted;
+      gameboy.PauseRequested += GameBoy_PauseRequested;
+      gameboy.StopRequested += GameBoy_StopRequested;
+    }
+
+    private void ReloadAPU()
+    {
+      _apu = _gameboy.APU;
       _waveFormat = new WaveFormat(_apu.SampleRate,
                                    _apu.SampleSize * 8,
                                    _apu.NumChannels);
-      // NOTE(Cristian): At 4400 bytes means 50 ms worth of audio.
-      //                 This low is for having low latency
-      //_source = new CircularWriteableBufferingSource(_waveFormat, 88 * 5000, 120);
       _source = new DirectStreamingSource(_waveFormat, _apu);
-
-      gameBoy.FrameCompleted += gameBoy_FrameCompleted;
-
       _soundOut = GetSoundOut();
       _soundOut.Initialize(_source);
       _soundOut.Volume = 0.2f;
-
-      sw = new Stopwatch();
-
-      gameBoy.PauseRequested += GameBoy_PauseRequested;
-      gameBoy.StopRequested += GameBoy_StopRequested;
     }
 
     private void GameBoy_PauseRequested()
@@ -52,7 +46,11 @@ namespace GBSharp.Audio
     {
       if(_soundOut.PlaybackState == PlaybackState.Playing)
       {
+        // The soundout will be reloaded after the first frame
+        // of gameplay passes
         _soundOut.Stop();
+        _soundOut.Dispose();
+        _soundOut = null;
       }
     }
 
@@ -61,74 +59,18 @@ namespace GBSharp.Audio
 
     }
 
-    Stopwatch sw;
-
-    private bool _firstRun = true;
-    int _prevRC = 0;
-
-
-    ulong _RCSum = 0;
-    ulong _RCSamples = 0;
-
-
-    int _prevWC = 0;
-    ulong _WCSum = 0;
-    ulong _WCSamples = 0;
-
-
     void gameBoy_FrameCompleted()
     {
-      #region REPORTING
-#if false
-
-      ulong avgRC = 0;
-      int RCDiff = _source.ReadCursor - _prevRC;
-      if (Math.Abs(RCDiff) < 5000)
+      if (_soundOut == null)
       {
-        _RCSum += (ulong)RCDiff;
-        ++_RCSamples;
-        if (_RCSamples != 0)
-        {
-          avgRC = _RCSum / _RCSamples;
-        }
+        ReloadAPU();
       }
 
-      ulong avgWC = 0;
-      int WCDiff = _source.WriteCursor - _prevWC;
-      if (Math.Abs(WCDiff) < 10000)
-      {
-        _WCSum += (ulong)WCDiff;
-        ++_WCSamples;
-        if (_WCSamples != 0)
-        {
-          avgWC = _WCSum / _WCSamples;
-        }
-      }
 
-      sw.Stop();
-
-      System.Console.Out.WriteLineAsync("MS: " + sw.ElapsedMilliseconds.ToString());
-
-      //System.Console.Out.WriteLine("MS {7} | RC: {0}, WC: {1} | RCDIFF: {2}, RCDIFFAVG: {3} | WCDIFF: {4}, WCDIFFAVG: {5} | TOTALDIFF: {6}",
-      //                             _source.ReadCursor, _source.WriteCursor,
-      //                             RCDiff, avgRC,
-      //                             WCDiff, avgWC,
-      //                             _source.WriteCursor - _source.ReadCursor,
-      //                             sw.ElapsedMilliseconds);
-
-      _prevRC = _source.ReadCursor;
-      _prevWC = _source.WriteCursor;
-
-      sw.Reset();
-      sw.Start();
-
-#endif
-      #endregion
-
-      if(_soundOut.PlaybackState != PlaybackState.Playing)
-      {
+      if (_soundOut.PlaybackState != PlaybackState.Playing) {
         _soundOut.Play();
       }
+   
     }
 
     private static ISoundOut GetSoundOut()
