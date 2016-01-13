4a36700dc36d84eaf2b037579e3c252994f1047f
Cristián Donoso C <cristiandonosoc@gmail.com>
2016-01-13 14:35:03 -0300

    Added a SoundEventQueue class
    This class will enable for the cpu to send events to the
    sound generating code. This will permit to separate the correct
    simulation of the sound hardware vs the actual sound. Right now
    they're entangled. Even though it works right in play, it completely
    breaks during debug gameboy stepping.

---
+++ GBSharp/AudioSpace/SoundEventQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBSharp.AudioSpace
+{
+  internal class SoundEventQueue
+  {
+    private int _readCursor = 0;
+    private int _writeCursor = 0;
+    private int _elementCount = 0;
+    private object _lockObj = new object();
+
+    private long[] _ticksBuffer;
+    private short[] _outputValuesBuffer;
+
+    private int _bufferSize = 1000;
+
+    internal SoundEventQueue()
+    {
+      _ticksBuffer = new long[_bufferSize];
+      _outputValuesBuffer = new short[_bufferSize];
+    }
+
+    public void Queue(long ticks, short outputValue)
+    {
+      lock(_lockObj)
+      {
+        if (_elementCount == _bufferSize) { throw new Exception("Event Queue overflow"); }
+        ++_writeCursor;
+        _ticksBuffer[_writeCursor] = ticks;
+        _outputValuesBuffer[_writeCursor++] = outputValue;
+        if(_writeCursor == _bufferSize) { _writeCursor = 0; }
+        ++_elementCount;
+      }
+    }
+
+    public bool GetNextEvent(ref int eventId, ref long ticks, ref short outputValue)
+    {
+      lock(_lockObj)
+      {
+        if (_elementCount == 0) { return false; }
+        // If we have an element waiting, we unqueue
+        ++_readCursor;
+        if(_readCursor == _bufferSize) { _readCursor = 0; }
+        --_elementCount;
+      }
+      eventId = _readCursor;
+      ticks = _ticksBuffer[_readCursor];
+      outputValue = _outputValuesBuffer[_readCursor];
+      return true;
+    }
+  }
+
+
+}
