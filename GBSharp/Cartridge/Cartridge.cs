695f74633971140e89bbbaaa9fb4dd981f5f87ce
Cristián Donoso C <cristiandonosoc@gmail.com>
2016-03-20 21:23:14 -0300

    Separated state for Cartridge

--- GBSharp/Cartridge/Cartridge.cs
+++ GBSharp/Cartridge/Cartridge.cs
@@ -6,108 +6,112 @@ using System.Threading.Tasks;
 
 namespace GBSharp.Cartridge
 {
-    public class Cartridge : ICartridge
+  public class Cartridge : ICartridge
+  {
+    public ushort MemoryChangedLow { get; private set; }
+    public ushort MemoryChangedHigh { get; private set; }
+
+    class State
     {
-        public ushort MemoryChangedLow { get; private set; }
-        public ushort MemoryChangedHigh { get; private set; }
-
-        byte[] rom;
-        int romSize;
-        int ramSize;
-        CartridgeType type;
-        string title;
-
-        /// <summary>
-        /// Loads the contents of the catridge rom from a byte array.
-        /// </summary>
-        public void Load(byte[] rom)
-        {
-            // Magic addresses!
-            ushort titleAddress = 0x0134;
-            ushort catridgeTypeAddress = 0x0147;
-            ushort ramSizeAddress = 0x0149;
-
-            // Save rom reference and parse headers
-            this.rom = rom;
-            this.romSize = this.rom.Length;
-            this.ramSize = 0;
-
-            if (this.rom.Length > ramSizeAddress)
-            {
-              switch (this.rom[ramSizeAddress])
-              {
-                case 0:
-                  ramSize = 0;
-                  break;
-                case 1:
-                  ramSize = 1 * 1024;
-                  break;
-                case 2:
-                  ramSize = 8 * 1024;
-                  break;
-                case 3:
-                  ramSize = 32 * 1024;
-                  break;
-                case 4:
-                  ramSize = 128 * 1024;
-                  break;
-              }
-            }
-
-            this.type = this.rom.Length > catridgeTypeAddress ? (CartridgeType)this.rom[catridgeTypeAddress] : CartridgeType.ROM_ONLY;
-            this.title = "";
-
-            if (rom.Length > titleAddress + 16)
-            {
-              for (var i = 0; i < 16; ++i)
-              {
-                if (this.rom[titleAddress + i] == 0)
-                {
-                  break;
-                }
-                this.title += (char)this.rom[titleAddress + i];
-              }
-            }
-        }
-
-        /// <summary>
-        /// See: CatridgeType enum.
-        /// </summary>
-        public CartridgeType Type
-        {
-            get { return this.type; }
-        }
-
-        /// <summary>
-        /// Contents of the rom.
-        /// </summary>
-        public byte[] Data
-        {
-            get { return this.rom; }
-        }
-
-        /// <summary>
-        /// Game title, max 16 characters.
-        /// </summary>
-        public string Title
-        {
-            get { return this.title; }
-        }
-
-        /// <summary>
-        /// ROM size in bytes.
-        /// </summary>
-        public int RomSize
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        /// <summary>
-        /// RAM size in bytes.
-        /// </summary>
-        public int RamSize
-        {
-            get { throw new NotImplementedException(); }
-        }
+      internal byte[] Rom;
+      internal int RomSize;
+      internal int RamSize;
+      internal CartridgeType Type;
+      internal string Title;
     }
+    State _state = new State();
+
+    /// <summary>
+    /// Loads the contents of the catridge rom from a byte array.
+    /// </summary>
+    public void Load(byte[] rom)
+    {
+      // Magic addresses!
+      ushort titleAddress = 0x0134;
+      ushort catridgeTypeAddress = 0x0147;
+      ushort ramSizeAddress = 0x0149;
+
+      // Save rom reference and parse headers
+      _state.Rom = rom;
+      _state.RomSize = _state.Rom.Length;
+      _state.RamSize = 0;
+
+      if (_state.Rom.Length > ramSizeAddress)
+      {
+        switch (_state.Rom[ramSizeAddress])
+        {
+          case 0:
+            _state.RamSize = 0;
+            break;
+          case 1:
+            _state.RamSize = 1 * 1024;
+            break;
+          case 2:
+            _state.RamSize = 8 * 1024;
+            break;
+          case 3:
+            _state.RamSize = 32 * 1024;
+            break;
+          case 4:
+            _state.RamSize = 128 * 1024;
+            break;
+        }
+      }
+
+      _state.Type = _state.Rom.Length > catridgeTypeAddress ? (CartridgeType)_state.Rom[catridgeTypeAddress] : CartridgeType.ROM_ONLY;
+      _state.Title = "";
+
+      if (rom.Length > titleAddress + 16)
+      {
+        for (var i = 0; i < 16; ++i)
+        {
+          if (_state.Rom[titleAddress + i] == 0)
+          {
+            break;
+          }
+          _state.Title += (char)_state.Rom[titleAddress + i];
+        }
+      }
+    }
+
+    /// <summary>
+    /// See: CatridgeType enum.
+    /// </summary>this
+    public CartridgeType Type
+    {
+      get { return _state.Type; }
+    }
+
+    /// <summary>
+    /// Contents of the rom.
+    /// </summary>
+    public byte[] Data
+    {
+      get { return _state.Rom; }
+    }
+
+    /// <summary>
+    /// Game title, max 16 characters.
+    /// </summary>
+    public string Title
+    {
+      get { return _state.Title; }
+    }
+
+    /// <summary>
+    /// ROM size in bytes.
+    /// </summary>
+    public int RomSize
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    /// <summary>
+    /// RAM size in bytes.
+    /// </summary>
+    public int RamSize
+    {
+      get { throw new NotImplementedException(); }
+    }
+  }
 }
