d977938f4a8c7c9a7715520e9f0d095846285bf1
Cristián Donoso C <cristiandonosoc@gmail.com>
2016-06-09 13:30:01 -0400

    Autohide of all the non-essential tools

--- GBSharp.View/GameBoyView.xaml.cs
+++ GBSharp.View/GameBoyView.xaml.cs
@@ -5,43 +5,63 @@ using Microsoft.Win32;
 
 namespace GBSharp.View
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    private readonly GameBoyViewModel _mainWindowViewModel;
-    private readonly IGameBoy _gameBoy;
-    private readonly KeyboardHandler _keyboardHandler;
-
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-      _gameBoy = new GameBoy();
-      _keyboardHandler = new KeyboardHandler();
-      _mainWindowViewModel = new GameBoyViewModel(_gameBoy, 
-                                                  new DispatcherAdapter(this), 
-                                                  new WindowAdapter(this), 
-                                                  new OpenFileDialogAdapterFactory(), 
-                                                  _keyboardHandler);
-      this.DataContext = _mainWindowViewModel;
-    }
+        private readonly GameBoyViewModel _mainWindowViewModel;
+        private readonly IGameBoy _gameBoy;
+        private readonly KeyboardHandler _keyboardHandler;
 
-    protected override void OnClosed(System.EventArgs e)
-    {
-      _mainWindowViewModel.OnClosed();
-      _gameBoy.Dispose();
-      base.OnClosed(e);
-    }
+        public MainWindow()
+        {
+            InitializeComponent();
+            _gameBoy = new GameBoy();
+            _keyboardHandler = new KeyboardHandler();
+            _mainWindowViewModel = new GameBoyViewModel(_gameBoy,
+                                                        new DispatcherAdapter(this),
+                                                        new WindowAdapter(this),
+                                                        new OpenFileDialogAdapterFactory(),
+                                                        _keyboardHandler);
+            this.DataContext = _mainWindowViewModel;
+        }
 
-    protected override void OnKeyDown(KeyEventArgs e)
-    {
-      _keyboardHandler.NotifyKeyDown(e);
-    }
+        protected override void OnClosed(System.EventArgs e)
+        {
+            _mainWindowViewModel.OnClosed();
+            _gameBoy.Dispose();
+            base.OnClosed(e);
+        }
 
-    protected override void OnKeyUp(KeyEventArgs e)
-    {
-      _keyboardHandler.NotifyKeyUp(e);
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            _keyboardHandler.NotifyKeyDown(e);
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            _keyboardHandler.NotifyKeyUp(e);
+        }
+
+        private void DockingManager_Loaded(object sender, RoutedEventArgs e)
+        {
+            Background.ToggleAutoHide();
+            Breakpoints.ToggleAutoHide();
+            CPU.ToggleAutoHide();
+            //DisplayTiming.ToggleAutoHide();
+            Interrupts.ToggleAutoHide();
+            InstructionHistogram.ToggleAutoHide();
+            IORegisters.ToggleAutoHide();
+            Memory.ToggleAutoHide();
+            MemoryImage.ToggleAutoHide();
+            //SoundChannelInternals.ToggleAutoHide();
+            //Spectogram.ToggleAutoHide();
+            SpriteLayer.ToggleAutoHide();
+            //Sprites.ToggleAutoHide();
+            SoundRecording.ToggleAutoHide();
+            TileMap.ToggleAutoHide();
+            Window.ToggleAutoHide();
+        }
     }
-  }
 }
